9d415bb83652af64c6f0337a55c7e3dabc943ce8
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-06-20 10:55:34 +0530

    added builder pattern

---
+++ Week-01/Design-Patterns/BuilderPatternExample/src/builder/BuilderPatternTest.java
@@ -0,0 +1,26 @@
+package builder;
+
+public class BuilderPatternTest {
+
+    public static void main(String[] args) {
+
+        Computer gamingPC = new Computer.Builder()
+                .setCPU("Intel i9")
+                .setRAM(32)
+                .setStorage(1000)
+                .setGPU("RTX 4080")
+                .build();
+
+        Computer officePC = new Computer.Builder()
+                .setCPU("Intel i5")
+                .setRAM(16)
+                .setStorage(512)
+                .build();
+
+        System.out.println("Gaming PC:");
+        gamingPC.displayConfiguration();
+
+        System.out.println("Office PC:");
+        officePC.displayConfiguration();
+    }
+}
