a58dbbe177bcb99ad3b96731bd6bb1bdb39c1dca
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-06-20 12:11:04 +0530

    Decorator Proxy  pattern added

---
+++ Week-01/Design-Patterns/DecoratorPatternExample/src/decorator/DecoratorPatternTest.java
@@ -0,0 +1,14 @@
+package decorator;
+
+public class DecoratorPatternTest {
+
+    public static void main(String[] args) {
+
+        Notifier notifier =
+                new SlackNotifierDecorator(
+                        new SMSNotifierDecorator(
+                                new EmailNotifier()));
+
+        notifier.send("System Maintenance Tonight");
+    }
+}
