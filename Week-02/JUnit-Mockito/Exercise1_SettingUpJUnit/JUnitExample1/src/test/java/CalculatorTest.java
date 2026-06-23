a829a0365a67dc685cc3af2118e31f203e8be4ea
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-06-23 23:02:43 +0530

    Exercise3_AssertionsinJUnit

--- Week-02/JUnit-Mockito/Exercise1_SettingUpJUnit/JUnitExample1/src/test/java/CalculatorTest.java
+++ Week-02/JUnit-Mockito/Exercise1_SettingUpJUnit/JUnitExample1/src/test/java/CalculatorTest.java
@@ -1,21 +1,3 @@
-//import static org.junit.Assert.assertEquals;
-//
-//import org.junit.jupiter.api.Test;
-//
-//public class CalculatorTest {
-//
-//    @Test
-//    public void testAdd() {
-//
-//        Calculator calc = new Calculator();
-//
-//        assertEquals(5,calc.add(2, 3)
-//        );
-//    }
-//}
-//
-
-
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
