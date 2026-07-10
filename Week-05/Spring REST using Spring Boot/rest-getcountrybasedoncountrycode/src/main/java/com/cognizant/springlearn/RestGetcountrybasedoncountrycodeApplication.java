f20a8e38502e8e7579a5ae6bd22eceb29aa0af10
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-07-10 22:12:50 +0530

    added REST - Get country based on country code

---
+++ Week-05/Spring REST using Spring Boot/rest-getcountrybasedoncountrycode/src/main/java/com/cognizant/springlearn/RestGetcountrybasedoncountrycodeApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGetcountrybasedoncountrycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestGetcountrybasedoncountrycodeApplication.class, args);
+	}
+
+}
