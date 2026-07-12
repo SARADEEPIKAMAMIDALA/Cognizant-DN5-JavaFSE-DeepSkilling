77a01bffd0ae3aa4bf0dc862395cf583bc139d3f
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-07-12 23:01:33 +0530

    added RESTservictogetallemployees

---
+++ Week-05/Spring REST using Spring Boot/3. spring-rest-handson/RESTservictogetallemployees/src/main/java/com/cognizant/springlearn/ResTservictogetallemployeesApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTservictogetallemployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTservictogetallemployeesApplication.class, args);
+	}
+
+}
