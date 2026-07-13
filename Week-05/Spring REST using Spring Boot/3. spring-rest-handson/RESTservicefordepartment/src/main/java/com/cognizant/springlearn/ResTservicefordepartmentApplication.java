54bfe40b122aef863e4b747f627647e729e41b63
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-07-13 22:37:20 +0530

    added RESTservicefordepartment

---
+++ Week-05/Spring REST using Spring Boot/3. spring-rest-handson/RESTservicefordepartment/src/main/java/com/cognizant/springlearn/ResTservicefordepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTservicefordepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTservicefordepartmentApplication.class, args);
+	}
+
+}
