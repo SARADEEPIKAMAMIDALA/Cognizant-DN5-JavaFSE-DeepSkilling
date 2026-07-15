2d869bd1ece51c49ab52abc15998274e3dac805e
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-07-15 19:46:00 +0530

    added Creating Microservices for account and loan

---
+++ Week-05/Microservices/microservices/account/src/main/java/com/cognizant/account/AccountApplication.java
@@ -0,0 +1,9 @@
+package com.cognizant.account;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication 
+public class AccountApplication{
+	public static void main(String[] a){
+		SpringApplication.run(AccountApplication.class,a);
+		}
+	}
---
+++ Week-05/Microservices/microservices/loan/src/main/java/com/cognizant/loan/LoanApplication.java
@@ -0,0 +1,9 @@
+package com.cognizant.loan;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication 
+public class LoanApplication{
+	public static void main(String[] a){
+		SpringApplication.run(LoanApplication.class,a);
+		}
+	}
