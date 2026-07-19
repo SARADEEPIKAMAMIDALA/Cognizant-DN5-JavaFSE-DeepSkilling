3a374534314d586588429c662cb1e5623298affc
saradeepika03@gmail.com <saradeepika03@gmail.com>
2026-07-19 20:04:43 +0530

    create an react directory

---
+++ Week-06/React/7. ReactJS-HOL/shoppingapp/src/Cart.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class Cart extends React.Component {
+
+    render() {
+
+        return (
+
+            <table border="1" cellPadding="8" align="center">
+
+                <thead>
+
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {
+                        this.props.item.map((item, index) => (
+
+                            <tr key={index}>
+
+                                <td>{item.itemname}</td>
+
+                                <td>{item.price}</td>
+
+                            </tr>
+
+                        ))
+                    }
+
+                </tbody>
+
+            </table>
+
+        );
+
+    }
+
+}
+
+export default Cart;
